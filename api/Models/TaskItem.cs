fbbe247fcaf46c451f473edbb363ba41f6f54758
Enock <bernardo.enock@vileve.com.br>
2025-09-12 20:24:14 -0300

    feat: Create Tasks Model

--- api/Models/TaskItem.cs
+++ api/Models/TaskItem.cs
@@ -1,17 +1,29 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.Models
 {
   public class TaskItem
   {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [MaxLength(100, ErrorMessage = "Title can have max 100 characters")]
     public string Title { get; set; } = string.Empty;
+
+    [MaxLength(500, ErrorMessage = "Description can have max 500 characters")]
     public string? Description { get; set; }
+
     public bool IsCompleted { get; set; } = false;
+
+    [Required(ErrorMessage = "Category is required")]
+    [MaxLength(50, ErrorMessage = "Category can have max 50 characters")]
     public string Category { get; set; } = string.Empty;
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
 
+    [Required]
     public int UserId { get; set; }
     public User User { get; set; } = null!;
   }
