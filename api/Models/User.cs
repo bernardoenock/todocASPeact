297db251e3d9f8d2ffeb232620d24d3f79e18793
Enock <bernardo.enock@vileve.com.br>
2025-09-12 20:24:29 -0300

    feat: Add validations on Model Users

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.Models
 {
   public class User
   {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Username is required")]
+    [MaxLength(50, ErrorMessage = "Username can have max 50 characters")]
     public string Username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "PasswordHash is required")]
     public string PasswordHash { get; set; } = string.Empty;
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
