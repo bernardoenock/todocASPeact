af4052a01bc3ab855d1b3e3589427fdd7b0ca121
Enock <bernardo.enock@vileve.com.br>
2025-09-12 14:45:36 -0300

    feat: initial api structure

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<TaskItem> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+    }
+  }
+}
