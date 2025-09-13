09d404c3f9ac458dfb428a3002c98792fd3f73ae
Enock <bernardo.enock@vileve.com.br>
2025-09-13 13:56:33 -0300

    feat: Add ApiSwitch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from '@/contexts/AuthContext'
+import { ApiProvider } from '@/contexts/ApiContext'
 import { ProtectedRoute } from '@/components/ProtectedRoute'
 import { LoginPage } from '@/pages/LoginPage'
 import { RegisterPage } from '@/pages/RegisterPage'
 import { TasksPage } from '@/pages/TasksPage'
+import { ApiSwitch } from '@/components/molecules/ApiSwitch'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,25 +17,31 @@ const queryClient = new QueryClient({
   },
 })
 
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route
-              path="/tasks"
-              element={
-                <ProtectedRoute>
-                  <TasksPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/" element={<Navigate to="/tasks" replace />} />
-          </Routes>
-        </BrowserRouter>
+        <ApiProvider>
+          <BrowserRouter>
+            <header className="p-4 flex justify-end">
+              <ApiSwitch />
+            </header>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route
+                path="/tasks"
+                element={
+                  <ProtectedRoute>
+                    <TasksPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Navigate to="/tasks" replace />} />
+            </Routes>
+          </BrowserRouter>
+        </ApiProvider>
       </AuthProvider>
     </QueryClientProvider>
   )
